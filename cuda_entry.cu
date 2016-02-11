f37dfc65565489351ca2b143939b96216099e957
Zhengchun Liu <liuzhengchun@gmail.com>
2016-02-11 17:56:26 -0500

    debug

--- cuda_entry.cu
+++ cuda_entry.cu
@@ -239,6 +239,7 @@ __global__ void evacuation_update(float *cnt, float *cap, float4 *pturn,
                 + (diff_bk - diff_cap);
         __syncthreads();
     }
+    return;
 // 3rd step, process halo synchronization!!!! synchronizing via device global memory    
 // to update, we have to know how much vehicle actully went out (get accepted by neighboor)
     int blk_uid = blockIdx.y*gridDim.x + blockIdx.x;
