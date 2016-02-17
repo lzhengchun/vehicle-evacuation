916c384b3ea5db11f1e0e501b1c4b9bf52be3db9
Zhengchun Liu <liuzhengchun@gmail.com>
2016-02-17 11:00:46 -0500

    remove redundancy

--- cuda_entry.cu
+++ cuda_entry.cu
@@ -357,32 +357,20 @@ __global__ void evacuation_update(float4 *p_vcnt_in, float4 *p_vcnt_out, float *
     if(threadIdx.x == 0){                                // left
         int id_helper = id_helper_st + 3*CUDA_BLOCK_SIZE + threadIdx.y;
         d_halo_sync[id_helper] = halo_sync[3][idy] - io[idy][0].y;      // number of vehicles which actully go out
-        if(d_halo_sync[id_helper] > VEHICLE_PER_STEP){
-            d_halo_sync[id_helper] = 0.888888888888;
-        }
     }      
     if(threadIdx.x == CUDA_BLOCK_SIZE-1){                // right
         int id_helper = id_helper_st + CUDA_BLOCK_SIZE + threadIdx.y;
-        d_halo_sync[id_helper] = halo_sync[1][idy] - io[idy][CUDA_BLOCK_SIZE+1].w;     
-        if(d_halo_sync[id_helper] > VEHICLE_PER_STEP){
-            d_halo_sync[id_helper] = 0.888888888888;
-        }          
+        d_halo_sync[id_helper] = halo_sync[1][idy] - io[idy][CUDA_BLOCK_SIZE+1].w;             
     }
 
     if(threadIdx.y == 0){                                // top
         int id_helper = id_helper_st + threadIdx.x;
-        d_halo_sync[id_helper] = halo_sync[0][idx] - io[0][idx].z;    
-        if(d_halo_sync[id_helper] > VEHICLE_PER_STEP){
-            d_halo_sync[id_helper] = 0.888888888888;
-        }           
+        d_halo_sync[id_helper] = halo_sync[0][idx] - io[0][idx].z;              
     }
 
     if(threadIdx.y == CUDA_BLOCK_SIZE-1){                // bottom
         int id_helper = id_helper_st + 2*CUDA_BLOCK_SIZE + threadIdx.x;
-        d_halo_sync[id_helper] = halo_sync[2][idx] - io[CUDA_BLOCK_SIZE+1][idx].x;     
-        if(d_halo_sync[id_helper] > VEHICLE_PER_STEP){
-            d_halo_sync[id_helper] = 0.888888888888;
-        }        
+        d_halo_sync[id_helper] = halo_sync[2][idx] - io[CUDA_BLOCK_SIZE+1][idx].x;           
     }    
 }
 /*
